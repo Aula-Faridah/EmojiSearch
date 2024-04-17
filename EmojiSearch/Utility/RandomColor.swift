e6d9ee9d27dd5a0975e149a2b081742bc3d7e15f
Aula Faridah <aulafaridah@gmail.com>
2024-04-17 10:23:18 +0700

    Addition RandomColor

---
+++ EmojiSearch/Utility/RandomColor.swift
@@ -0,0 +1,18 @@
+//
+//  RandomColor.swift
+//  EmojiSearch
+//
+//  Created by MacBook Pro on 17/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct RandomColor {
+    static func bgView() -> Color {
+        return Color(red: Double.random(in: 0...1),
+                     green: Double.random(in: 0...1),
+                     blue: Double.random(in: 0...1))
+    }
+}
