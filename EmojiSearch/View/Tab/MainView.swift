a960447f5f9fa653d216e318faccc1dd700db40d
Aula Faridah <aulafaridah@gmail.com>
2024-04-18 09:26:54 +0700

    TabView and DisclosureGroup

---
+++ EmojiSearch/View/Tab/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  EmojiSearch
+//
+//  Created by MacBook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView{
+            ContentView()
+                .tabItem {
+                    Label("Emoji", systemImage: "ev.plug.dc.nacs")
+                }
+            
+            Text("Map View")
+                .tabItem {
+                    Label("Location", systemImage: "binoculars" )
+                }
+                .badge(4)
+            
+            Text("Setting Page")
+                .tabItem {
+                    Label("Settings", systemImage: "gear") }
+            
+            
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
